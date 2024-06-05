a985363476b88f780951ad195a2a60ae98194666
”Dmytro <ipz203_gds@student.ztu.edu.ua>
2024-06-06 02:16:03 +0300

    Added a template for sending messages

--- services/email-service.js
+++ services/email-service.js
@@ -11,11 +11,71 @@ class EmailService {
       debug: true,
     });
 
+    const htmlTemplate = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Аукціон речей Bid&Win</title>
+
+        <style>
+            body {
+                background: linear-gradient(to right, #ff7e5f, #feb47b);
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+            }
+            .card {
+                width: 80%;
+                max-width: 600px;
+                background: linear-gradient(to bottom, white, lightblue);
+                border-radius: 10px;
+                box-shadow: 1px 2px 15px 2px rgba(0, 0, 0, 0.2); 
+                padding: 20px;
+                text-align: center;
+                border: 1px solid grey;
+            }
+            .logo {
+                width: 300px;
+                height: 300px;
+            }
+            .heading {
+                font-size: 24px;
+                font-weight: bold;
+                margin-bottom: 20px;
+            }
+            .content {
+                font-size: 16px;
+                line-height: 1.5;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="card">
+            <img src="http://localhost:3000/logo.png" alt="Auction Bid&Win Logo" class="logo">
+            
+            <h1 class="heading">Аукціон речей Bid&Win</h1>
+
+            <div class="content">
+                ${text}
+                <p>Детальніше про наш сайт: http://localhost:3000/</p>
+            </div>
+        </div>
+    </body>
+    </html>`;
+
+    const htmlContent = htmlTemplate.replace("%content%", text);
+
     const mailOptions = {
       from: process.env.EMAIL,
       to,
       subject,
-      text,
+      html: htmlContent,
     };
 
     try {

5ee309a4679ad504e216a050cf9c5915a552bf07
”Dmytro <ipz203_gds@student.ztu.edu.ua>
2024-06-08 03:30:20 +0300

    An updated method for getting all lots with the ability to filter, search, sort and paginate.

--- controllers/lot-controller.js
+++ controllers/lot-controller.js
@@ -1,4 +1,5 @@
 const { Lot, Bid, User, Category, AuctionHistory } = require("../models");
+const { Op } = require("sequelize");
 const HttpError = require("../errors/http-error");
 
 const lotService = require("../services/lot-service");
@@ -294,12 +295,110 @@ class LotController {
 
   async getAllLots(req, res, next) {
     try {
-      // const { sortBy, sortOrder, page, limit } = req.query;
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+      const offset = (page - 1) * limit;
+      const sortBy = req.query.sortBy;
+      const currentPriceFrom = parseFloat(req.query.currentPriceFrom);
+      const currentPriceTo = parseFloat(req.query.currentPriceTo);
+      const buyNowPriceFrom = parseFloat(req.query.buyNowPriceFrom);
+      const buyNowPriceTo = parseFloat(req.query.buyNowPriceTo);
+      const dateOption = req.query.dateOption;
+      const categoryIds = req.query.categoryId
+        ? req.query.categoryId.split(",")
+        : [];
+      const statuses = req.query.status ? req.query.status.split(",") : [];
+      const search = req.query.search;
 
-      // const pageNum = isNaN(page) || page < 1 ? 1 : parseInt(page);
-      // const limitNum = isNaN(limit) || limit < 1 ? 10 : parseInt(limit);
+      let order = [];
 
-      const lots = await Lot.findAll({
+      if (sortBy === "price_asc") {
+        order = [["currentPrice", "ASC"]];
+      } else if (sortBy === "price_desc") {
+        order = [["currentPrice", "DESC"]];
+      } else if (sortBy === "created_asc") {
+        order = [["createdAt", "ASC"]];
+      } else if (sortBy === "created_desc") {
+        order = [["createdAt", "DESC"]];
+      } else if (sortBy === "end_asc") {
+        order = [["endDate", "ASC"]];
+      } else if (sortBy === "end_desc") {
+        order = [["endDate", "DESC"]];
+      } else if (sortBy === "buy_now_asc") {
+        order = [["buyNowPrice", "ASC"]];
+      } else if (sortBy === "buy_now_desc") {
+        order = [["buyNowPrice", "DESC"]];
+      }
+
+      let where = {};
+
+      if (categoryIds.length > 0) {
+        where.categoryId = {
+          [Op.in]: categoryIds,
+        };
+      }
+
+      if (statuses.length > 0) {
+        where.status = {
+          [Op.in]: statuses,
+        };
+      }
+
+      if (!isNaN(currentPriceFrom) && !isNaN(currentPriceTo)) {
+        where.currentPrice = {
+          [Op.between]: [currentPriceFrom, currentPriceTo],
+        };
+      } else if (!isNaN(currentPriceFrom)) {
+        where.currentPrice = { [Op.gte]: currentPriceFrom };
+      } else if (!isNaN(currentPriceTo)) {
+        where.currentPrice = { [Op.lte]: currentPriceTo };
+      }
+
+      if (!isNaN(buyNowPriceFrom) && !isNaN(buyNowPriceTo)) {
+        where.buyNowPrice = { [Op.between]: [buyNowPriceFrom, buyNowPriceTo] };
+      } else if (!isNaN(buyNowPriceFrom)) {
+        where.buyNowPrice = { [Op.gte]: buyNowPriceFrom };
+      } else if (!isNaN(buyNowPriceTo)) {
+        where.buyNowPrice = { [Op.lte]: buyNowPriceTo };
+      }
+
+      if (dateOption && dateOption !== "all") {
+        const currentDate = new Date();
+        let startDateLot = new Date(currentDate);
+        let endDateLot = new Date();
+
+        if (dateOption === "24_hours") {
+          endDateLot = new Date(currentDate);
+          endDateLot.setHours(endDateLot.getHours() + 24);
+        } else if (dateOption === "7_days") {
+          endDateLot = new Date(currentDate);
+          endDateLot.setDate(endDateLot.getDate() + 7);
+        } else if (dateOption === "30_days") {
+          endDateLot = new Date(currentDate);
+          endDateLot.setDate(endDateLot.getDate() + 30);
+        }
+
+        where.endDate = {
+          [Op.between]: [startDateLot, endDateLot],
+        };
+      }
+
+      if (dateOption === "recently_sold") {
+        const currentDate = new Date();
+        const startDateLot = new Date(currentDate);
+        const endDateLot = new Date(currentDate);
+        endDateLot.setDate(endDateLot.getDate() - 7);
+
+        where.endDate = {
+          [Op.between]: [endDateLot, startDateLot],
+        };
+      }
+
+      if (search) {
+        where.title = { [Op.like]: `%${search}%` };
+      }
+
+      const { rows: lots, count: totalItems } = await Lot.findAndCountAll({
         include: [
           {
             model: User,
@@ -311,16 +410,22 @@ class LotController {
             include: [{ model: User }],
           },
         ],
-        // order: [[sortBy || "createdAt", sortOrder || "DESC"]],
-        // offset: (pageNum - 1) * limitNum,
-        // limit: limitNum || 10,
+        where,
+        limit,
+        offset,
+        order,
       });
 
-      // const total = await Lot.count();
       await lotService.closeLotArray(lots);
 
-      res.status(200).json(lots);
+      res.status(200).json({
+        lots: lots,
+        totalItems,
+        totalPages: Math.ceil(totalItems / limit),
+        currentPage: page,
+      });
     } catch (error) {
+      console.log(error.message);
       next(
         HttpError.internalServerError(
           "Не вдалося отримати лоти. Будь ласка, спробуйте пізніше."
